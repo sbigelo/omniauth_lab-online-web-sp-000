380612f23cdb08bd0fd59bbd0fb232662be17728
sbigelo <sean.bigelow1@gmail.com>
2021-03-08 15:40:34 -0500

    commit tree 1ff79e7725ff83fa39753256c0fe1aa0f7489e3a

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   
   root 'welcome#home'
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  get '/auth/facebook/callback' => 'sessions#create'
 end
